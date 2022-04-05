03f1c02494266507540eba2c17755fc139317776
Srivatsan Sridhar <srivatsan@Srivatsans-MacBook-Air.local>
2022-04-04 21:50:47 -0700

    HW1: Error handling and data logging in network

--- network.ts
+++ network.ts
@@ -66,8 +66,13 @@ export function connectAsServer(bootstrapMode=false){
 		});
 
 		socket.on('data', data => {
+			console.log("Data received from "+socket.remoteAddress+" port "+socket.remotePort+": "+data)
 			msgHandler.handle(data.toString());
 		});
+
+		socket.on('error', error => {
+			console.log("Connection error with "+socket.remoteAddress+" port "+socket.remotePort+": "+ error)
+		})
 	});
 
 	server.on('error',function(error){
@@ -114,10 +119,11 @@ export async function connectAsClient(){
 			});
 
 			client.on('error', (error) => {
-				console.log('Error: ' + error);
+				console.log("Connection error with "+client.remoteAddress+" port "+client.remotePort+": "+ error)
 			});
 
 			client.on('data', data => {
+				console.log("Data received from "+client.remoteAddress+" port "+client.remotePort+": "+data)
 				msgHandler.handle(data.toString());
 			});
 
@@ -146,6 +152,13 @@ export function sendMessage(data:string, peer:Peer){
 export function broadcastMessage(data:string){
 	connectedPeerList.forEach((peer,index) => {
 		sendMessage(data,peer);
+	});
+}
+
+export function broadcastMessageExceptSender(data:string, sender:Peer){
+	connectedPeerList.forEach((peer,index) => {
+		if(peer!==sender)
+			sendMessage(data,peer);
 	});
 }
 
@@ -181,9 +194,13 @@ export async function readDiscoveredPeers(){
 	}
 }
 
-export function closeDueToError(peer:Peer, error:string){
+export function reportError(peer:Peer, error:string){
 	console.log(error);
 	sendMessage(Message.encodeMessage({type:"error", error:error}), peer)
+}
+
+export function closeDueToError(peer:Peer, error:string){
+	reportError(peer, error)
 	setTimeout(() => {
 		peer.socket.destroy();
 	}, 500)
